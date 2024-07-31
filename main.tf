d1e15785f9a09c3bf1ff5f6b206327ea5508ff06
ccie7599 <bapley@akamai.com>
2024-07-31 08:25:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "linode_instance" "linode" {
   region      = element(var.regions, count.index)
   type        = "g6-standard-1"
   image       = "linode/ubuntu24.04"
-  tags        = "workshop"
+  tags        = toset(["workshop"])
   authorized_keys = [local.sanitized_ssh_key]
 }
 
